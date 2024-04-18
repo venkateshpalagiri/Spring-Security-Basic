ccfd8707c365868bf4ed527b65a8f573369395bd
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-04-17 23:21:36 -0400

    init set-up

---
+++ src/test/java/com/venkatesh/security/SpringSecurityDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venkatesh.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
